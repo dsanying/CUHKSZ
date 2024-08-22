656d7b7d9d9f64dee1723ac3891f84fc956f1ba2
暗影 <119548583@qq.com>
2024-08-22 12:25:25 +0800

    Site updated: 2024-08-22 12:25:25

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["CEC1000/","CSC1004/","CSC1003/","DDA2001/","MAT1002/","ENG1001/","IT-guide/","ENG1002/","MAT2041/","PHY1001/","contact-school/","about/","course-evaluation/","STA2001/","timeline/","map/"];function toRandomPost(){
+var posts=["DDA2001/","CEC1000/","CSC1003/","ENG1001/","ENG1002/","CSC1004/","IT-guide/","MAT1002/","PHY1001/","about/","MAT2041/","STA2001/","contact-school/","course-evaluation/","timeline/","map/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
