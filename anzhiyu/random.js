9f53900351c5c18842a286d081a3945e49826638
暗影 <119548583@qq.com>
2024-08-23 18:24:32 +0800

    Site updated: 2024-08-23 18:24:32

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["CHM1001/","CEC1000/","CEC2000/","CSC1001/","CSC1002/","CSC1003/","ENG1001/","ENG1002/","CSC1004/","DDA2001/","MAT1001/","GFN1000/","IT-guide/","MAT2041/","MAT1002/","MAT2040/","PHY1001/","PED/","contact-school/","STA2001/","about/","lgu-guide-1/","course-evaluation/","map/","lgu-guide-2/","lgu-guide-3/","timeline/"];function toRandomPost(){
+var posts=["CHM1001/","CEC1000/","CEC2000/","CSC1003/","DDA2001/","MAT1001/","IT-guide/","MAT2041/","MAT1002/","PHY1001/","PED/","contact-school/","STA2001/","about/","lgu-guide-1/","course-evaluation/","map/","lgu-guide-2/","lgu-guide-3/","timeline/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
